9522e0650cd235f1f51532e5c07e3dfe89886921
evelardiez <evelardiez@EMILIOVELARB903>
2018-10-13 02:00:23 +0200

    Agregar archivos de proyecto.

---
+++ WinFormsGrids/RadFormGridView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace TelerikWinFormsAppGrid
+{
+    public partial class RadFormGridView : Telerik.WinControls.UI.RadForm
+    {
+        public RadFormGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            var connectionString =
+                System.Configuration.ConfigurationManager.
+                ConnectionStrings["NwindConnectionString"].ConnectionString;
+
+            var connection = new OleDbConnection(connectionString);
+
+            connection.Open();
+
+            string queryString = "SELECT * from Employees";
+
+            var adapter = new OleDbDataAdapter(queryString, connection);
+
+            var dtAccess = new DataTable();
+            var bindingSource = new BindingSource();
+            bindingSource.DataMember = "Employees";
+            bindingSource.DataSource = dtAccess;
+
+            this.dataGridView1.DataSource = bindingSource;
+
+            using (new OleDbCommandBuilder(adapter))
+            {
+                adapter.Fill(dtAccess);
+            }
+
+            this.dataGridView1.AutoResizeColumns();
+        }
+    }
+}
---
+++ WinFormsGrids/RadFormRadGrid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace TelerikWinFormsAppGrid
+{
+    public partial class RadFormRadGrid : Telerik.WinControls.UI.RadForm
+    {
+        public RadFormRadGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void RadForm2_Load(object sender, EventArgs e)
+        {
+            var connectionString =
+                System.Configuration.ConfigurationManager.
+                ConnectionStrings["NwindConnectionString"].ConnectionString;
+
+            var connection = new OleDbConnection(connectionString);
+
+            connection.Open();
+
+            string queryString = "SELECT * from Employees";
+
+            var adapter = new OleDbDataAdapter(queryString, connection);
+
+            var dtAccess = new DataTable();
+            var bindingSource = new BindingSource();
+            bindingSource.DataMember = "Employees";
+            bindingSource.DataSource = dtAccess;
+
+            this.radGridView1.DataSource = bindingSource;
+
+            using (new OleDbCommandBuilder(adapter))
+            {
+                adapter.Fill(dtAccess);
+            }
+
+            radGridView1.BestFitColumns();
+        }
+    }
+}
